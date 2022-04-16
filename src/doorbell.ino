4a5bfa1dc19d84deda06f485f4ef8a8fbd70f286
Mark Blashki <markblashki1@gmail.com>
2022-04-17 00:25:57 +1000

    fix: increase generality update to only send rising edge events

--- src/doorbell.ino
+++ src/doorbell.ino
@@ -1,6 +1,15 @@
 #include <MQTT.h>
+#include "Debounce/Debounce.h"
+
+Debounce doorbellDebounced = Debounce();
 
 const String url = "192.168.0.26";
+const String topic = "efm/sensors/doorbell";
+const String deviceName = "MBlashkiArgon";
+const String MQTTusername = "MBlashkiArgon";
+const String MQTTpassword = "1234";
+const pin_t Doorbell = D6;
+const pin_t LED = D7;
 
 // Declarations
 void callback(char* topic, byte* payload, unsigned int length);
@@ -15,14 +24,13 @@ void callback(char* topic, byte* payload, unsigned int length) {
   
   Serial.println("Callback");
   Serial.println(p);
-  delay(1000);
 }
 
 
 void connect(MQTT & client){
   // connect to the server(unique id by Time.now())
-  client.connect("MBlashkiArgon_" + String(Time.now()), "MBlashkiArgon", "1234");
-  client.subscribe("inTopic/message");
+  client.connect(deviceName + "_" + String(Time.now()), MQTTusername, MQTTpassword);
+  client.subscribe(topic);
 }
 
 // Setup
@@ -30,17 +38,39 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Start!");
   RGB.control(true);
-
+  pinMode(LED, OUTPUT);
+  pinMode(Doorbell, INPUT_PULLDOWN);
+  doorbellDebounced.attach(Doorbell, INPUT_PULLDOWN);
+  doorbellDebounced.interval(100);
 }
 
+bool isConnected = false;
 // Loop
 void loop() {
-  if (client.isConnected()){
+  if(client.isConnected() && !isConnected){ // Just connected
+    Serial.println("Connected.");
+  }
+  
+  isConnected = client.isConnected();
+  
+  if (isConnected){
+    // Loop MQTT
     client.loop();
-    // publish/subscribe
-    Serial.println("Publish");
-    client.publish("inTopic/message","hello world");
-    delay(30000);
+    
+    // Check Doorbell
+    if(doorbellDebounced.update()){ // Doorbell changed state
+      if(doorbellDebounced.read() == 1){ // Only publish a rising edge
+        Serial.println("Doorbell!");
+        
+        digitalWrite(LED, HIGH);
+        delay(200);
+        digitalWrite(LED, LOW);
+        delay(200);
+        
+        // Publish doorbell
+        client.publish(topic, "1");
+      }
+    }
   }
   else{
     Serial.println("Disconnected! Trying to reconnect now...");

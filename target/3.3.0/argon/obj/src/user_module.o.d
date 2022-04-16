a33ec2e638e2a385094dcdf5146abe0c7fcc3422
Mark Blashki <markblashki1@gmail.com>
2022-04-16 21:24:48 +1000

    chore: remove unneccisary files fom source control

--- target/3.3.0/argon/obj/src/user_module.o.d
+++
@@ -1,6 +0,0 @@
-d\:/DEVELOPMENT/Radio_Eastern/particle_argon/doorbell/target/3.3.0/argon//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
-../../../dynalib/inc/dynalib.h:
